f4f2a97684bd80da2471dcef5e17ac6f1ca6391c
Kevlyn Kadamala <kevlyn@gmail.com>
2021-03-29 18:22:56 +0530

    Added functions for audio command

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AudioCommand from './src/screen/AudioCommand';
 import ObjectDescriptionScreen from './src/screen/ObjectDescription';
 
 export default function App() {
   return (
-    <ObjectDescriptionScreen />
+    <AudioCommand />
   );
 }
 
--- src/utils/index.js
+++ src/utils/index.js
@@ -2,5 +2,6 @@ import { getDescription } from './getDescription';
 import { read } from './read';
 import { takePicture } from './takePicture';
 import { pickImage } from './pickImage';
+import { command } from './command';
 
-export { getDescription, takePicture, pickImage, read };
+export { getDescription, takePicture, pickImage, read, command };
