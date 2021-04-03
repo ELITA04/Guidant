2e8e45644ca8c17491f9e900cb7230477d3323f9
Kevlyn Kadamala <kevlyn@gmail.com>
2021-04-03 14:06:44 +0530

    Integrated help page

--- App.js
+++ App.js
@@ -5,10 +5,12 @@ import { createMaterialTopTabNavigator } from '@react-navigation/material-top-ta
 import Home from './src/screen/Home';
 import Describe from './src/screen/ObjectDescription';
 import Read from './src/screen/Read';
+import Help from './src/screen/Help';
+import { MaterialIcons } from '@expo/vector-icons';
 
 const Tab = createMaterialTopTabNavigator();
 
-const screenOptions = {
+const tabBarOptions = {
   activeTintColor: '#ffffff',
   inactiveTintColor: '#AA3939',
   showIcon: true,
@@ -17,7 +19,7 @@ const screenOptions = {
   tabStyle: {
     paddingTop: 32,
     borderWidth: 1,
-    borderColor: '#550000'
+    borderColor: '#550000',
   },
   indicatorStyle: {
     backgroundColor: '#801515',
@@ -26,16 +28,37 @@ const screenOptions = {
   style: {
     backgroundColor: '#FFAAAA',
   },
-  labelStyle: { fontSize: 27, }
+  labelStyle: { fontSize: 15, },
+  iconStyle: { height: 40, width: 40 }
 }
+
+const screenOptions = ({ route }) => ({
+  tabBarIcon: ({ focused, color, size }) => {
+    let iconName;
+
+    if (route.name === 'Home') {
+      iconName = 'home';
+    } else if (route.name === 'Describe') {
+      iconName = 'preview';
+    } else if (route.name === 'Read') {
+      iconName = 'plagiarism';
+    } else if (route.name === 'Help') {
+      iconName = 'help';
+    }
+
+    // You can return any component that you like here!
+    return <MaterialIcons name={iconName} size={40} color={color} style={{ textAlign: 'center' }} />;
+  },
+});
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator initialRouteName="Home" tabBarOptions={screenOptions}>
+      <Tab.Navigator initialRouteName="Home" screenOptions={screenOptions} tabBarOptions={tabBarOptions}>
         < Tab.Screen name="Home" component={Home} />
         <Tab.Screen name="Describe" component={Describe} />
         <Tab.Screen name="Read" component={Read} />
+        <Tab.Screen name="Help" component={Help} />
       </Tab.Navigator>
     </NavigationContainer>
   );
